3544a90955780917a8b1eecce0295765079b158a
Zihao Feng <zihao.feng@outlook.com>
2023-08-31 17:22:55 +0800

    Update util.py

--- environment/util.py
+++ environment/util.py
@@ -1,7 +1,8 @@
 import networkx as nx
 from random import choice
-from helper.graph import compute_path_length,  compute_flow_value
+from helper.graph import compute_path_length, compute_flow_value
 from typing import Tuple
+
 
 # reads data from a .brite file and generates graphs
 def create_graph(numNodes=100, numEdges=200, fileName="Waxman.brite") -> nx.Graph:
@@ -19,7 +20,7 @@ def create_graph(numNodes=100, numEdges=200, fileName="Waxman.brite") -> nx.Grap
         q = f.readline().strip()
         line = q.split("\t")
         g.add_edge(int(line[1]), int(line[2]),
-                   weight=float(line[4]), capacity=float(line[5])/100)
+                   weight=float(line[4]), capacity=float(line[5]) / 100)
     return g
 
 
@@ -43,26 +44,47 @@ def get_new_route(graph: nx.Graph) -> Tuple[int, int]:
     # node2 = 20
     return node1, node2
 
+
 # get new route try catch safe. To be used when nodes are removed from network
 
 
-def _get_new_route(graph: nx.Graph) -> Tuple[int, int]:
-    nodes = list(graph.nodes)
-    done = False
-    node1 = -1
-    node2 = -1
-    while not done:
-        try:
-            node1 = choice(nodes)
-            node2 = choice(nodes)
-            while node1 == node2:
-                node2 = choice(nodes)
-            nx.shortest_path(graph, node1, node2)
-            done = True
-        except:
-            done = False
+from random import choice
+from typing import Tuple
+import networkx as nx
 
-    return node1, node2
+
+def _get_new_route(graph: nx.Graph) -> Tuple[int, int]:
+    """Find a pair of unique nodes in the graph such that a path exists between them."""
+
+    # List all nodes in the graph
+    nodes = list(graph.nodes)
+
+    # Initialize control flag and node variables
+    route_found = False
+    source_node = None
+    target_node = None
+
+    while not route_found:
+        try:
+            # Randomly select source and target nodes
+            source_node = choice(nodes)
+            target_node = choice(nodes)
+
+            # Ensure the source and target nodes are different
+            while source_node == target_node:
+                target_node = choice(nodes)
+
+            # Check if a path exists between the source and target nodes
+            nx.shortest_path(graph, source_node, target_node)
+
+            # If a path exists, set the control flag to True
+            route_found = True
+
+        except nx.NetworkXNoPath:
+            # Catch the specific 'No Path' exception and continue the loop
+            route_found = False
+
+    return source_node, target_node
 
 
 def get_flows(graph: nx.Graph, num_flows: int) -> Tuple[int, int]:
@@ -72,25 +94,27 @@ def get_flows(graph: nx.Graph, num_flows: int) -> Tuple[int, int]:
         paths.append(nx.shortest_path(graph, s, t))
     return paths
 
-# adjust the latency and bandwidth to simulate traffic in the network
-
 
 def adjust_lat_band(graph: nx.Graph, paths: list):
+    """
+    Adjust the latency and bandwidth to simulate traffic in the network
+    """
     edges = []
+    # loops through the paths
     for path in paths:
-        for i in range(len(path)-1):
-            if ((path[i], path[i+1])) not in edges and ((path[i+1], path[i])) not in edges:
-                edges.append((path[i], path[i+1]))
+        # loops through the nodes in the current path
+        for i in range(len(path) - 1):
+            # add edge to list of edges
+            if (path[i], path[i + 1]) not in edges and (path[i + 1], path[i]) not in edges:
+                edges.append((path[i], path[i + 1]))
             # increase latency
-            graph[path[i]][path[i+1]]["weight"] = graph[path[i]
-                                                        ][path[i+1]]["weight"] ** .3
-            if graph[path[i]][path[i+1]]["weight"] > 0.999:
-                graph[path[i]][path[i+1]]["weight"] = 0.999
+            graph[path[i]][path[i + 1]]["weight"] = graph[path[i]][path[i + 1]]["weight"] ** .3
+            if graph[path[i]][path[i + 1]]["weight"] > 0.999:
+                graph[path[i]][path[i + 1]]["weight"] = 0.999
             # decrease bandwidth
-            graph[path[i]][path[i+1]]["capacity"] = (graph[path[i]
-                                                           ][path[i+1]]["capacity"])**1.2
-            if graph[path[i]][path[i+1]]["capacity"] < 0.001:
-                graph[path[i]][path[i+1]]["capacity"] = 0.001
+            graph[path[i]][path[i + 1]]["capacity"] = (graph[path[i]][path[i + 1]]["capacity"]) ** 1.2
+            if graph[path[i]][path[i + 1]]["capacity"] < 0.001:
+                graph[path[i]][path[i + 1]]["capacity"] = 0.001
     return graph
 
 
